33df44447d87c8afafcbce19b9ab2d83e0c7dfcd
Miguel Varela <miangel5718@gmail.com>
2023-05-08 15:12:32 -0500

    Data Context

--- Data/SumpermarketContext.cs
+++ Data/SumpermarketContext.cs
@@ -1,6 +1,14 @@
-﻿namespace SupermarketWEB.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
 {
-    public class SumpermarketContext
+    public class SumpermarketContext : DbContext
     {
+        public SumpermarketContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> categories { get; set; }
     }
 }
---
+++ Models/Detail.cs
@@ -0,0 +1,13 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Detail
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public int ProductUd { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+        public ICollection<Invoice>? Invoices { get; set; } = default!;
+    }
+}
